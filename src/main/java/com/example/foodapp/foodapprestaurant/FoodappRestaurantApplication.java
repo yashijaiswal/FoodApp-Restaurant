3b3b540de0f002d872fe1487cf85369b35d37ce3
Yashi Jaiswal <yashijaiswal411@gmail.com>
2022-05-16 21:50:46 +0530

    Adding Swagger config

--- src/main/java/com/example/foodapp/foodapprestaurant/FoodappRestaurantApplication.java
+++ src/main/java/com/example/foodapp/foodapprestaurant/FoodappRestaurantApplication.java
@@ -2,12 +2,28 @@ package com.example.foodapp.foodapprestaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FoodappRestaurantApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodappRestaurantApplication.class, args);
+	}
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.example.foodapp.foodapprestaurant.controller")).paths(PathSelectors.any())
+				.build();
 	}
 
 }
